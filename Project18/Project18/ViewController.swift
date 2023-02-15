6eeceab3c18d6c8ab6f525ac374e1d2843e10036
Emre <78293131+emrebkr@users.noreply.github.com>
2023-02-15 18:39:06 +0300

    Project 18 is added.

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Emre Bakır on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i),")
+            
+        }
+    }
+
+
+}
+
